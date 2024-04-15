e15e4cfb3ec847749bb7652014a0e93d9ae560fc
Marcos Almorox <marcos.almorox@gmail.com>
2024-04-15 10:54:01 +0200

    cambios pequeños

--- django/django/urls.py
+++ django/django/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('chistes/', include('chistes.urls'))
+    path('chistes/', include('chistes.urls')),
+    path('fp/', include('fp.urls'))
 ]
--- django/fp/views.py
+++ django/fp/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import FamiliaProfesional
 
-# Create your views here.
+def listado_familias(request):
+    familias = FamiliaProfesional.objects.all()
+    return render(request, 'fp/listado_familias.html', {'familias': familias})
+
+def detalle_ciclos(request, familia_id):
+    familia = get_object_or_404(FamiliaProfesional, pk=familia_id)
+    ciclos = familia.ciclo_set.all()
+    return render(request, 'fp/detalle_ciclos.html', {'familia': familia, 'ciclos': ciclos})
