ec25776e6284a276d39537bc79caba720f72358d
Marcos Almorox <73890028+malmorox@users.noreply.github.com>
2024-04-17 16:20:47 +0200

    Delete django/fp directory

--- django/fp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.listado_familias, name='listado_familias'),
-    path('<int:familia_id>/', views.detalle_ciclos, name='detalle_ciclos'),
-]
