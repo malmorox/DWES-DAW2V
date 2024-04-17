4b0c49db2e5a0dd26ad481d2c481d5a740f92d80
Marcos Almorox <73890028+malmorox@users.noreply.github.com>
2024-04-17 16:21:13 +0200

    Delete django/chistes directory

--- django/chistes/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'chistes'
-
-urlpatterns = [
-    path('', views.ChisteListView.as_view(), name='listado_chistes'),
-]
--- django/chistes/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.views import generic
-from .models import Chiste
-
-
-class ChisteListView(generic.ListView):
-    model = Chiste
-    template_name = 'chistes/listado_chistes.html'
