d9967a979310ffc940d076418034c611a6faa644
Marcos Almorox <marcos.almorox@gmail.com>
2024-05-17 00:37:28 +0200

    cambios antes dormir

--- django/repaso/coches/views.py
+++ django/repaso/coches/views.py
@@ -1,28 +1,46 @@
+from django.shortcuts import render
 from django.views import generic
 from .models import Coche
+from .forms import DatosUsuarioForm
+import math
 
 
 class ListadoCochesView(generic.ListView):
-    model = Coche
     template_name = 'coches/listado_coches.html'
     context_object_name = 'coches'
+    
+    def get_queryset(self):
+        return Coche.objects.all()
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = DatosUsuarioForm()
+        context['resultados'] = None
+        return context
+    
+    def post(self, request, *args, **kwargs):
+        form = DatosUsuarioForm(request.POST)
+        context = self.get_context_data()
+        if form.is_valid():
+            sueldo_mensual = form.cleaned_data['sueldo_mensual']
+            gasto_alquiler = form.cleaned_data['gasto_alquiler']
+            gastos_mensuales = form.cleaned_data['gastos_mensuales']
+            ahorro_mensual = sueldo_mensual - gasto_alquiler - gastos_mensuales
+            
+            coches = self.get_queryset()
+            resultados = {}
+            for coche in coches:
+                if ahorro_mensual > 0:
+                    meses_necesarios = math.ceil(coche.precio / ahorro_mensual)
+                    anos = meses_necesarios // 12
+                    meses = meses_necesarios % 12
+                    resultados[coche.pk] = {'anos': anos, 'meses': meses}
+            context['resultados'] = resultados
+        context['form'] = form
+        return render(request, self.template_name, context)
 
 
 class DetalleCocheView(generic.DetailView):
     model = Coche
     template_name = 'coches/detalle_coche.html'
     context_object_name = 'coche'
-
-
-def FormularioDatosUsuario(request):
-    if request.method == 'POST':
-        form = DatosUsuarioForm(request.POST)
-        if form.is_valid():
-            sueldo_mensual = form.cleaned_data['sueldo_mensual']
-            gasto_alquiler = form.cleaned_data['gasto_alquiler']
-            gastos_mensuales = form.cleaned_data['gastos_mensuales']
-            gasto_total = sueldo_mensual - gasto_alquiler - gastos_mensuales
-            return render(request, 'coches/resultado.html', {'gasto_total': gasto_total})
-    else:
-        form = DatosUsuarioForm()
-    return render(request, 'coches/formulario_datos_usuario.html', {'form': form})
