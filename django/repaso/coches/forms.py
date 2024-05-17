8448ff40eb2f0e2499175ba48e83fc2509a5364f
Marcos Almorox <marcos.almorox@gmail.com>
2024-05-17 19:58:59 +0200

    cambios por hoy

--- django/repaso/coches/forms.py
+++ django/repaso/coches/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 
-
 class DatosUsuarioForm(forms.Form):
     sueldo_mensual = forms.DecimalField(label='Sueldo neto mensual', max_digits=10, decimal_places=2, min_value=0)
     gasto_alquiler = forms.DecimalField(label='Gasto en alquiler', max_digits=10, decimal_places=2, min_value=0)
--- django/repaso/coches/views.py
+++ django/repaso/coches/views.py
@@ -1,13 +1,15 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from django.views import generic
 from .models import Coche
 from .forms import DatosUsuarioForm
 import math
 
 
-class ListadoCochesView(generic.ListView):
+class ListadoCochesView(generic.ListView, generic.edit.FormMixin):
+    model = Coche
     template_name = 'coches/listado_coches.html'
     context_object_name = 'coches'
+    form_class = DatosUsuarioForm
     
     def get_queryset(self):
         return Coche.objects.all()
@@ -19,25 +21,29 @@ class ListadoCochesView(generic.ListView):
         return context
     
     def post(self, request, *args, **kwargs):
-        form = DatosUsuarioForm(request.POST)
+        form = self.get_form()
         context = self.get_context_data()
         if form.is_valid():
             sueldo_mensual = form.cleaned_data['sueldo_mensual']
             gasto_alquiler = form.cleaned_data['gasto_alquiler']
             gastos_mensuales = form.cleaned_data['gastos_mensuales']
-            ahorro_mensual = sueldo_mensual - gasto_alquiler - gastos_mensuales
+            ahorro_mensual = sueldo_mensual - (gasto_alquiler + gastos_mensuales)
             
             coches = self.get_queryset()
+            self.object_list = coches
             resultados = {}
             for coche in coches:
                 if ahorro_mensual > 0:
                     meses_necesarios = math.ceil(coche.precio / ahorro_mensual)
-                    anos = meses_necesarios // 12
-                    meses = meses_necesarios % 12
-                    resultados[coche.pk] = {'anos': anos, 'meses': meses}
+                    anos_ahorrar = meses_necesarios // 12
+                    meses_ahorrar = meses_necesarios % 12
+                    
+                    #coche.anos = anos_ahorrar
+                    #coche.meses = meses_ahorrar
+                    #coche.save()
+                    resultados[coche.pk] = {'anos': anos_ahorrar, 'meses': meses_ahorrar}
             context['resultados'] = resultados
-        context['form'] = form
-        return render(request, self.template_name, context)
+        return self.render_to_response(context)
 
 
 class DetalleCocheView(generic.DetailView):
